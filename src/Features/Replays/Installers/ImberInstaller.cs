84657e92d67a517d6f7aae84192620b1f9e4efde
Riley <speecil@speecil.dev>
2026-06-16 00:02:33 +1000

    Replays: stop imber ui being togglable in fpfc

--- src/Features/Replays/Installers/ImberInstaller.cs
+++ src/Features/Replays/Installers/ImberInstaller.cs
@@ -1,17 +1,20 @@
 ﻿using ScoreSaber.Features.Replays.UI;
+using SiraUtil.Tools.FPFC;
 using Zenject;
 
 namespace ScoreSaber.Features.Replays.Installers {
     internal class ImberInstaller : Installer {
         private readonly ReplayState _replayState;
+        private readonly IFPFCSettings _fpfcSettings;
 
-        public ImberInstaller(ReplayState replayState) {
+        public ImberInstaller(ReplayState replayState, IFPFCSettings fpfcSettings) {
             _replayState = replayState;
+            _fpfcSettings = fpfcSettings;
         }
 
         public override void InstallBindings() {
 
-            if (_replayState.IsPlaybackEnabled && !_replayState.IsLegacyReplay) {
+            if (_replayState.IsPlaybackEnabled && !_replayState.IsLegacyReplay && !_fpfcSettings.Enabled) {
                 Container.BindInterfacesTo<ImberManager>().AsSingle();
                 Container.BindInterfacesAndSelfTo<ImberScrubber>().AsSingle();
                 Container.BindInterfacesAndSelfTo<ImberSpecsReporter>().AsSingle();
